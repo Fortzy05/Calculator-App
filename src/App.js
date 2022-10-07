160a4105bc1a2048a028bf15de2ac8ee1807a106
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-10-07 15:45:41 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Calculator App</h1>
+      <p>Calculator App</p>
+    </div>
+  );
+}
+
+export default App;
